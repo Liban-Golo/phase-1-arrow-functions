43aeee90f9cb993b7c1aa8af14771c753e83df96
Liban Golo <liban.golo@student.moringaschool.com>
2025-09-15 11:10:19 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const add = function(a, b) {
+  return a + b;
+}
+
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => {
+  const result = a * b;
+  return result;
+}
+
+const divide = function() {
+  return 2000 / 100;
+}
+const square = y => y * y;
